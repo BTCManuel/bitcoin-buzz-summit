8c3a674f5a033056350d088aca69021e9dc95893
JoeWauki <87571013+JoeWauki@users.noreply.github.com>
2025-05-15 16:53:07 +0200

    Update Speakers.tsx

--- src/components/Speakers.tsx
+++ src/components/Speakers.tsx
@@ -74,7 +74,7 @@ const Speakers = () => {
     },
     {
       name: "Joe Martin",
-      role: "Co-Founder_test_test...",
+      role: "Co-Founder_test_test.83..",
       company: "Bitcoin Forum_test",
       image: "/images/speakers/Joe_Martin.jpg",
       bio: "ist als Journalist, Keynote-Speaker und Buchautor mit über 30 Jahren Berufserfahrung in der Welt der Technologie einer der wichtigsten Bitcoin-Erklärer Deutschlands. Er hat auch die Volksbank Raiffeisenbank Bayern Mitte bei der Einführung des Angebots von Bitcoin und Bitcoin-Lösungen begleitet und unterstützt. Joe Martin ist Co-Founder und Hauptansprechpartner für alle Speaker beim 3. BitcoinForum. Kontaktdaten: https://linktr.ee/JoeMartinBTC",
