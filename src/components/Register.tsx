d74e67f65aeaa1ae0b2f497e863b7b1f2a1e1f99
BTCManuel <hauk.manuel@gmail.com>
2025-05-16 09:26:18 +0200

    Update Register.tsx

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -91,7 +91,7 @@ const Register = () => {
                 <img 
                   src="/images/ticket/rabatt.png" 
                   alt="Frühbucher Rabatt 21%-50%" 
-                  className="w-[300px] h-[300px] object-contain rounded-full rotate-[30deg] animate-pulse"
+                  className="w-[600px] h-[600px] object-contain rounded-full rotate-[45deg] animate-pulse"
                   style={{
                     animation: "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite"
                   }}
