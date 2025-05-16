a723ccd98ddb73b6ac8fee63bcf5ef7148db4676
JoeMartinBTC <joemartin@nena-celeste.de>
2025-05-16 14:07:06 +0200

    Update Speakers.tsx
    link

--- src/components/Speakers.tsx
+++ src/components/Speakers.tsx
@@ -50,7 +50,7 @@ const Speakers = () => {
       role: "Co-Founder",
       company: "Bitcoin Forum",
       image: "/images/speakers/Joe_Martin.jpg",
-      bio: "ist als Journalist, Keynote-Speaker und Buchautor mit über 30 Jahren Berufserfahrung in der Welt der Technologie einer der wichtigsten Bitcoin-Erklärer Deutschlands. Er hat auch die Volksbank Raiffeisenbank Bayern Mitte bei der Einführung des Angebots von Bitcoin und Bitcoin-Lösungen begleitet und unterstützt. Joe Martin ist Co-Founder und Hauptansprechpartner für alle Speaker beim 3. BitcoinForum. <br> <a href='https://linktr.ee/JoeMartinBTC' target='_blank'>https://linktr.ee/JoeMartinBTC</a>",
+      bio: "ist als Journalist, Keynote-Speaker und Buchautor mit über 30 Jahren Berufserfahrung in der Welt der Technologie einer der wichtigsten Bitcoin-Erklärer Deutschlands. Er hat auch die Volksbank Raiffeisenbank Bayern Mitte bei der Einführung des Angebots von Bitcoin und Bitcoin-Lösungen begleitet und unterstützt. Joe Martin ist Co-Founder und Hauptansprechpartner für alle Speaker beim 3. BitcoinForum. [https://linktr.ee/JoeMartinBTC](https://linktr.ee/JoeMartinBTC)",
       bgColor: "bg-[#FEF7CD]/50",
       bgSolidColor: "bg-[#FEF7CD]"
     },
