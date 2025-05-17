bd09cc7b06ea17dbbf348d62f1c2c9e37e6fd5dd
BTCManuel <hauk.manuel@gmail.com>
2025-05-17 13:28:07 +0200

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -82,7 +82,7 @@ const About = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {features.map((feature, index) => (
             <Card 
               key={index} 
