78f447916a07c804db5c1ec63087bcce83481802
JoeMartinBTC <joemartin@nena-celeste.de>
2025-05-17 10:42:50 +0200

    Update GlobalPopup.tsx
    formatiert

--- src/components/GlobalPopup.tsx
+++ src/components/GlobalPopup.tsx
@@ -6,8 +6,27 @@ const GlobalPopup = () => {
   const { open, data, closePopup } = usePopup();
 
   const defaultTitle = <strong>Zuerst ein Ticket, dann die volle Auswahl</strong>;
-  const defaultMessage = "Für den Eintritt zur Saturnarena ist immer ein Ticket erforderlich – auch bei kostenlosen Messetickets. Danach können Sie sich Ihre Wunsch-Workshops sichern, sobald diese angeboten werden. Sie können dafür einen Alarm beim Ticketkauf setzen.  Der Ticketkauf wird von der Volksbank Raiffeisenbank Bayern Mitte e.G., die auch der Veranstalter der 3. BitcoinForum ist durchgeführt – 100 % DSGVO-konform und in wenigen Schritten erledigt. Je nach Ticketkategorie genießen Sie Zugang zur Ausstellung, spannenden Vorträgen oder exklusiv zum VIP-Bereich mit zusätzlichen Vorträgen, Kamingesprächen, voller Verpflegung und weiteren Extras. Und das Beste: Nach dem Ticketkauf können Sie Ihr Erlebnis mit Workshops erweitern – von Einsteiger-Sessions bis zu technischen Masterclasses. Sichern Sie sich jetzt Ihr Ticket und gestalten Sie dann Ihr ganz persönliches Programm!";
-
+ const defaultMessage = (
+  <>
+    <p>
+      Für den Eintritt zur Saturnarena ist immer ein Ticket erforderlich – auch bei kostenlosen Messetickets.
+      Danach können Sie sich Ihre Wunsch-Workshops sichern, sobald diese verfügbar sind. Beim Ticketkauf können Sie dafür bequem einen Alarm setzen.
+    </p>
+    <p>
+      Der Ticketkauf erfolgt über die <strong>Volksbank Raiffeisenbank Bayern Mitte eG</strong>, die zugleich Veranstalter des <strong>3. Bitcoin Forums</strong> ist – 100 % DSGVO-konform und in wenigen Schritten erledigt.
+    </p>
+    <p>
+      Je nach Ticketkategorie genießen Sie Zugang zur Ausstellung, spannenden Vorträgen oder exklusiv zum VIP-Bereich
+      mit zusätzlichen Vorträgen, Kamingesprächen, voller Verpflegung und weiteren Extras.
+    </p>
+    <p>
+      <strong>Und das Beste:</strong> Nach dem Ticketkauf können Sie Ihr Erlebnis mit Workshops erweitern – von Einsteiger-Sessions bis hin zu technischen Masterclasses.
+    </p>
+    <p>
+      <strong>Sichern Sie sich jetzt Ihr Ticket</strong> und gestalten Sie Ihr ganz persönliches Programm!
+    </p>
+  </>
+);
   if (!open || !data) return null;
 
   return (
