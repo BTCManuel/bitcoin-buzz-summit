a859b2379ed94603fb5afd30e13ae350464474b4
BTCManuel <hauk.manuel@gmail.com>
2025-05-21 10:34:12 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Workshop from "./pages/Workshop";
 import ScrollToTop from "./pages/ScrollToTop";
 import GlobalPopup from "@/components/GlobalPopup";
 import { PopupProvider } from "@/pages/PopupContext";
-import CookieBanner from "@/components/CookieBanner";
-import Datenschutz from "./pages/Datenschutz";
-import BIP25Members from "./pages/BIP25Members";
-import FAQ from "./pages/FAQ";
-import WorkshopBitcoinInDerBank from "./pages/WorkshopBitcoinInDerBank";
 
 const queryClient = new QueryClient();
 
@@ -24,20 +18,15 @@ const App = () => (
       <Toaster />
       <Sonner />
       <PopupProvider>
-        <BrowserRouter>
+        <Router>
           <GlobalPopup />
           <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/workshop" element={<Workshop />} />
-            <Route path="/datenschutz" element={<Datenschutz />} />
-            <Route path="/bip25-members" element={<BIP25Members />} />
-            <Route path="/FAQ" element={<FAQ />} />
-            <Route path="/workshop/bitcoin-in-der-bank" element={<WorkshopBitcoinInDerBank />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-          <CookieBanner />
-        </BrowserRouter>
+        </Router>
       </PopupProvider>
     </TooltipProvider>
   </QueryClientProvider>
